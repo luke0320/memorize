4d787513d4b693f249123fdb48b686b943aae5c9
Luke Lee <lukelee011@outlook.com>
2022-01-16 22:07:40 +0800

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by luke mbp14 on 2022/1/16.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
